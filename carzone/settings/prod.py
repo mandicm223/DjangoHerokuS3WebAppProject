f66aec0f5a9acc87f95ec37ae0d9f081ab8f6c0f
JaSamLudiMoskri <78329923+JaSamLudiMoskri@users.noreply.github.com>
2022-03-15 13:07:30 +0100

    trying without staticfiles=False

--- carzone/settings/prod.py
+++ carzone/settings/prod.py
@@ -70,7 +70,7 @@ LOGGING = {
 
 # Heroku
 
-django_on_heroku.settings(locals() , staticfiles=False)
+django_on_heroku.settings(locals())
 del DATABASES['default']['OPTIONS']['sslmode']
 
 SITE_ID = 1
