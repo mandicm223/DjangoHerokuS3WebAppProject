fc538d07c95b54ee6336c56c2b6487e0c931f15a
JaSamLudiMoskri <78329923+JaSamLudiMoskri@users.noreply.github.com>
2022-03-15 12:30:51 +0100

    initial commit

---
+++ carzone/settings/dev.py
@@ -0,0 +1,20 @@
+from .base import *
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-yn8mu3=(jjkmbzcnw(t1nlmxx7ohu4alnr+-yie)f(ffb=jkfl'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = ['*']
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/4.0/howto/static-files/
+
+STATIC_URL = 'static/'
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+MEDIA_URL = 'media/'
+
+MEDIA_ROOT =  BASE_DIR / 'media'
---
+++ pages/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from .models import Team
+# Create your views here.
+
+def home(request):
+    teams = Team.objects.all()
+    data = {
+        'teams': teams
+    }
+    return render(request , 'pages/home.html' , data)
